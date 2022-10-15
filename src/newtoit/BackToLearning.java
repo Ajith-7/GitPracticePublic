30d2c8392916b180a56f0c6f94432a8939eb293f
Ajith-7 <cvsajith420@gmail.com>
2022-10-15 11:56:00 +0530

    Build is Ready

---
+++ src/newtoit/BackToLearning.java
@@ -0,0 +1,21 @@
+package newtoit;
+
+public class BackToLearning {
+	
+	private void mymethod() {
+		
+		System.out.println("CVSAJITH");
+	}
+	
+	public static void main(String[] args) {
+	 
+        BackToLearning object = new BackToLearning();
+        
+        object.mymethod();
+	
+		
+	}
+	
+	
+	
+}
